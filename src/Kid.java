ae3294f79087a1597a909529be84cffd703cf95c
Doiae9 <leonardogh75@gmail.com>
2022-03-24 11:57:32 -0600

    Terminados las clases padre hijo y main

--- src/Kid.java
+++ src/Kid.java
@@ -1,3 +1,26 @@
-public class Kid {
+public class Kid extends Personaje{
+
+    //colorPuerta
+    private String colorPuerta;
+    //reir
+    //llorar
+
+    public String getColorPuerta() {
+        return colorPuerta;
+    }
+
+    public void setColorPuerta(String colorPuerta) {
+        this.colorPuerta = colorPuerta;
+    }
+
+    public Kid(String nombre, double estatura, double peso, String colorPuerta) {
+        super(nombre, estatura, peso);
+        this.colorPuerta = colorPuerta;
+        System.out.println("Estoy en el constructor de Kid");
+    }
+
+    public void reir(){System.out.println("hahaha");}
+    public void llorar(){System.out.println("Buaahh!");}
+
 
 }
--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,8 @@ public class Main {
         Monstruo Sully = new Monstruo("Sully", 3.2f, 1000, true, false, "Azul",
                 new ArrayList<>(Arrays.asList("fuerte", "Carismatico", "trabajador")));
 
+    Kid boo = new Kid("Boo",0.50f,20.0f,"blanca con flores");
+    Sully.espantar();
+    boo.llorar();
     }
 }
